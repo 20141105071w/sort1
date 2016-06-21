6f9b980f58f74f506ebb12e2ec90055a78125bef
20141105071 <20141105071wyr@mac26.local>
2016-06-21 10:25:56 +0800

    sort
    首先定义一个数组，并且为数组赋值；之后分别定义变量i、j、temp，并赋初值为零。然后是冒泡排序，将数组中所有数字从小到大排序，用了for循环语
    句和if条件语句，if 条件语句中有赋值语句  。最后按顺序输出。

--- sort1/main.swift
+++ sort1/main.swift
@@ -7,6 +7,24 @@
 //
 
 import Foundation
+var arr = [20 , 2 , 10 ,6 , 8 , 9 , 11 , 1 , 99 , 112 , 0 , 3 , 12 , 15 , 16 , 18 , 70 , 50 , 60 , 100]
+var i = 0
+var j = 0
+var temp = 0
+for j = 0;j<arr.count+1;j++
 
+{
+    for i = 0;i<arr.count-1-j;i++
+{
+    if(arr[i]>arr[i+1]){
+        temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+        
+    }
+    
+}
+}
+print(arr)
 print("Hello, World!")
 
